a7c468ab9eae1b46644d17aab7dbbd9780c94829
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2024-02-02 10:33:55 +0100

    add api call

--- src/lib/electricity-maps/index.ts
+++ src/lib/electricity-maps/index.ts
@@ -1,26 +1,28 @@
+/* eslint-disable prettier/prettier */
 import * as dotenv from 'dotenv';
+import axios from 'axios';
 
 import {ERRORS} from '../../util/errors';
 import {buildErrorMessage} from '../../util/helpers';
 
-import {ModelParams} from '../../types/common';
+import {KeyValuePair, ModelParams} from '../../types/common';
 import {ModelPluginInterface} from '../../interfaces';
 
-const {AuthorizationError} = ERRORS;
 
+const {AuthorizationError, APIRequestError} = ERRORS;
 
 const BASE_URL = 'https://api.electricitymap.org/v3';
 
 export class ElectricityMapsModel implements ModelPluginInterface {
-    authorizationHeader: string = '';
+    authorizationHeader = '';
     errorBuilder = buildErrorMessage(ElectricityMapsModel);
     async configure(): Promise<ModelPluginInterface> {
-        this.initAuth();
-        return this;
+            this.initAuth();
+            return this;
     }
     async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
-        const outputs: ModelParams[] = [];
-        return outputs;
+            const outputs: ModelParams[] = [];
+            return outputs;
     }
     initAuth() {
         dotenv.config();
@@ -34,5 +36,31 @@ export class ElectricityMapsModel implements ModelPluginInterface {
             );
         }
         this.authorizationHeader = `auth-token: ${token}`;
+    }
+    private async get_carbon_intensity(zone_key?: string, latitude?: number, longitude?: number): Promise<KeyValuePair> {
+        // TODO handle parameters
+        const url = `${BASE_URL}/carbon-intensity/latest`;
+        const response = await axios.get(
+            url,
+            {
+                headers: {
+                    'auth-token': this.authorizationHeader,
+                },
+            }
+        )
+        if (response.status !== 200) {
+            throw new APIRequestError(
+                this.errorBuilder({
+                  message: `Error fetching data from WattTime API: ${JSON.stringify(
+                    response.status
+                  )}`,
+                })
+              );
+        }
+        const data = response.data;
+        return {
+            key: 'carbon_intensity',
+            value: data.carbonIntensity,
+        };
     }
 }
