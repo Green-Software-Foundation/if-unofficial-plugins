3608c7ca3ea8f92b1ff9077c44166cc642dcac11
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2024-02-29 23:51:34 +0100

    finish ratios and outputs

--- src/lib/electricity-maps/index.ts
+++ src/lib/electricity-maps/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import * as dotenv from 'dotenv';
+import * as dayjs from 'dayjs';
 import axios from 'axios';
 
 import {ERRORS} from '../../util/errors';
@@ -9,7 +10,7 @@ import {KeyValuePair, ModelParams} from '../../types/common';
 import {ModelPluginInterface} from '../../interfaces';
 
 
-const {AuthorizationError, APIRequestError} = ERRORS;
+const {AuthorizationError, InputValidationError, APIRequestError} = ERRORS;
 
 const BASE_URL = 'https://api.electricitymap.org/v3';
 
@@ -22,7 +23,40 @@ export class ElectricityMapsModel implements ModelPluginInterface {
     }
     async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
             const outputs: ModelParams[] = [];
-            return outputs;
+            return inputs.map((model_param)=>{
+                let unit = 'gCO2eq/kWh';
+                if (!model_param.power_consumption) {
+                    unit = 'gCO2eq';
+                }
+                const start = dayjs(model_param.timestamp);
+                const end = start.add(model_param.duration, 'second');
+                const carbon_intensities = this.get_carbon_intensity(model_param.longitude, model_param.latitude, start, end);
+                const hours = Math.floor(model_param.duration / 3600);
+                const blocs = [...Array(hours).keys()]
+                // Calculate for each full hour the ratio of the hour that is in the bloc
+                const hourly_ratios = blocs.map((hour) => {
+                    // this is the first hourly bloc, the ratio is the time between the start and the next hour.
+                    if (hour === 0) {
+                        return start.add(1, 'hour').startOf('hour').diff(start, 'second') / 3600;
+                    }
+                    // this is the last hourly bloc, the ratio is the time between the start of the hour and the end.
+                    if (hour === hours - 1) {
+                        return end.diff(start.add(hour, 'hour').startOf('hour'), 'second') / 3600;
+                    }
+                    return 1;
+                });
+                let total_carbon_intensity = 0;
+                carbon_intensities.forEach(
+                    (carbon_intensity, index)=>{
+                        total_carbon_intensity += carbon_intensity.value * hourly_ratios[index];
+                    }
+                )
+                return {
+                    ...model_param,
+                    carbon_intensity: total_carbon_intensity,
+                    unit: unit
+                }
+            });
     }
     initAuth() {
         dotenv.config();
@@ -37,30 +71,48 @@ export class ElectricityMapsModel implements ModelPluginInterface {
         }
         this.authorizationHeader = `auth-token: ${token}`;
     }
-    private async get_carbon_intensity(zone_key?: string, latitude?: number, longitude?: number): Promise<KeyValuePair> {
-        // TODO handle parameters
-        const url = `${BASE_URL}/carbon-intensity/latest`;
-        const response = await axios.get(
+    private get_carbon_intensity(longitude: number, latitude: number, start: dayjs.Dayjs, end: dayjs.Dayjs): KeyValuePair[] {
+        const parameters = {
+            lon: longitude,
+            lat: latitude,
+            start: start,
+            end: end
+        }
+        if (end.diff(start, 'days') > 10) {
+            throw InputValidationError(
+                this.errorBuilder({
+                    message: 'The maximum duration is 10 days for the Emaps API.',
+                    scope: 'input',
+                })
+            )
+        }
+        const url = `${BASE_URL}/carbon-intensity/past-range`;
+        const response = axios.get(
             url,
             {
+                params: parameters,
                 headers: {
                     'auth-token': this.authorizationHeader,
-                },
+                }
             }
         )
-        if (response.status !== 200) {
-            throw new APIRequestError(
-                this.errorBuilder({
-                  message: `Error fetching data from WattTime API: ${JSON.stringify(
-                    response.status
-                  )}`,
+        response.then(
+            (response)=>{
+                if (response.status !== 200) {
+                    throw new APIRequestError(
+                        this.errorBuilder({
+                            message: `Error: ${JSON.stringify(response.status)}`,
+                        })
+                    );
+                }
+                const data: KeyValuePair[] = response.data;
+                return data.map((carbon_intensity_data) => {
+                    return {
+                        datetime: carbon_intensity_data.datetime,
+                        value: carbon_intensity_data.carbonIntensity,
+                    }
                 })
-              );
-        }
-        const data = response.data;
-        return {
-            key: 'carbon_intensity',
-            value: data.carbonIntensity,
-        };
+            }
+        )
     }
 }
