584f4cf0cfd41a094581310b60b8f6029472ee45
alexzurbonsen <alexander.zur.bonsen@tngtech.com>
2024-03-09 01:24:49 +0100

    feat(lib): allow config and globalConfig parameters as input
    Signed-off-by: alexzurbonsen <alexander.zur.bonsen@tngtech.com>

--- src/lib/co2js/index.ts
+++ src/lib/co2js/index.ts
@@ -4,7 +4,7 @@ import {z} from 'zod';
 import {PluginInterface} from '../../interfaces';
 import {ConfigParams, PluginParams} from '../../types';
 
-import {allDefined, validate} from '../../util/validations';
+import {validate} from '../../util/validations';
 import {buildErrorMessage} from '../../util/helpers';
 import {ERRORS} from '../../util/errors';
 
@@ -13,23 +13,26 @@ const {InputValidationError} = ERRORS;
 export const Co2js = (globalConfig?: ConfigParams): PluginInterface => {
   const metadata = {kind: 'execute'};
   const errorBuilder = buildErrorMessage(Co2js.name);
-
   /**
    * Executes the plugin for a list of input parameters.
    */
   const execute = async (inputs: PluginParams[], config?: ConfigParams) => {
+    const validatedConfig = validateConfig(config);
     const mergedValidatedConfig = Object.assign(
       {},
-      validateConfig(config),
+      validatedConfig,
       validateGlobalConfig()
     );
     const model = new co2({model: mergedValidatedConfig.type});
 
     return inputs.map(input => {
+      const validatedInput = validateInput(input);
+      validateGreenWebHostExists(validatedConfig, validatedInput);
+
       const mergedWithConfig = Object.assign(
         {},
-        validateInput(input),
-        mergedValidatedConfig
+        mergedValidatedConfig,
+        validatedInput
       );
       const result = calculateResultByParams(mergedWithConfig, model);
 
@@ -68,55 +71,52 @@ export const Co2js = (globalConfig?: ConfigParams): PluginInterface => {
     return paramType[inputWithConfig.type]();
   };
 
+  const bytesSchema = z.object({
+    'network/data/bytes': z.number().optional(),
+    'network/data': z.number().optional(),
+  });
+  const greenWebHostSchema = z.object({
+    'green-web-host': z.boolean().optional(),
+  });
+  const globalConfigSchema = z.object({
+    options: z
+      .object({
+        dataReloadRatio: z.number().min(0).max(1).optional(),
+        firstVisitPercentage: z.number().min(0).max(1).optional(),
+        returnVisitPercentage: z.number().min(0).max(1).optional(),
+        gridIntensity: z
+          .object({
+            device: z
+              .number()
+              .or(z.object({country: z.string()}))
+              .optional(),
+            dataCenter: z
+              .number()
+              .or(z.object({country: z.string()}))
+              .optional(),
+            networks: z
+              .number()
+              .or(z.object({country: z.string()}))
+              .optional(),
+          })
+          .optional(),
+      })
+      .optional(),
+  });
+
   /**
-   * Validates input parameters.
+   * Validates input and config parameters.
    */
   const validateInput = (input: PluginParams) => {
-    const schema = z
-      .object({
-        'network/data/bytes': z.number(),
-        'network/data': z.number(),
-      })
-      .partial()
+    const inputSchema = bytesSchema
+      .merge(greenWebHostSchema)
+      .merge(globalConfigSchema)
       .refine(data => !!data['network/data/bytes'] || !!data['network/data'], {
         message:
           'Either `network/data/bytes` or `network/data` should be provided in the input.',
       });
 
-    return validate<z.infer<typeof schema>>(schema, input);
-  };
-
-  /**
-   * Validates Global config parameters.
-   */
-  const validateGlobalConfig = () => {
-    const schema = z.object({
-      options: z
-        .object({
-          dataReloadRatio: z.number().min(0).max(1).optional(),
-          firstVisitPercentage: z.number().min(0).max(1).optional(),
-          returnVisitPercentage: z.number().min(0).max(1).optional(),
-          gridIntensity: z
-            .object({
-              device: z
-                .number()
-                .or(z.object({country: z.string()}))
-                .optional(),
-              dataCenter: z
-                .number()
-                .or(z.object({country: z.string()}))
-                .optional(),
-              networks: z
-                .number()
-                .or(z.object({country: z.string()}))
-                .optional(),
-            })
-            .optional(),
-        })
-        .optional(),
-    });
-
-    return validate<z.infer<typeof schema>>(schema, globalConfig || {});
+    return validate<z.infer<typeof inputSchema>>(inputSchema, input);
   };
 
   /**
@@ -134,13 +134,37 @@ export const Co2js = (globalConfig?: ConfigParams): PluginInterface => {
     const schema = z
       .object({
         type: z.enum(['1byte', 'swd']),
-        'green-web-host': z.boolean(),
       })
-      .refine(allDefined, {
-        message: '`type` and `green-web-host` are not provided in node config',
+      .merge(greenWebHostSchema)
+      .refine(data => data['type'] !== undefined, {
+        message: '`type` must be provided in node config',
       });
 
     return validate<z.infer<typeof schema>>(schema, config);
+  };
+
+  /**
+   * Validates Global config parameters.
+   */
+  const validateGlobalConfig = () => {
+    return validate<z.infer<typeof globalConfigSchema>>(
+      globalConfigSchema,
+      globalConfig || {}
+    );
+  };
+
+  const validateGreenWebHostExists = (
+    config: ConfigParams,
+    input: PluginParams
+  ) => {
+    if (
+      config['green-web-host'] === undefined &&
+      input['green-web-host'] === undefined
+    ) {
+      throw new InputValidationError(
+        `Green web host is provided neither in config nor in input.\nConfig: ${config}\nInput: ${input}`
+      );
+    }
   };
 
   return {
