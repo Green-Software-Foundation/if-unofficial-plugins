ea47532b58cd7d6879297f8681b416f35ee6e64f
alexzurbonsen <alexander.zur.bonsen@tngtech.com>
2024-03-09 01:24:49 +0100

    feat(lib): allow config and globalConfig parameters as input
    Signed-off-by: alexzurbonsen <alexander.zur.bonsen@tngtech.com>

--- src/__tests__/unit/lib/co2js/index.test.ts
+++ src/__tests__/unit/lib/co2js/index.test.ts
@@ -123,6 +123,41 @@ describe('lib/co2js: ', () => {
         ]);
       });
 
+      it('returns a result when config properties are provided in input.', async () => {
+        const config = {type: 'swd'};
+        const inputs = [
+          {
+            timestamp: '2021-01-01T00:00:00Z',
+            duration: 3600,
+            'network/data/bytes': 100000,
+            'green-web-host': false,
+            options: {
+              dataReloadRatio: 0.6,
+              firstVisitPercentage: 0.9,
+              returnVisitPercentage: 0.1,
+            },
+          },
+        ];
+        const result = await output.execute(inputs, config);
+
+        expect.assertions(1);
+
+        expect(result).toStrictEqual([
+          {
+            timestamp: '2021-01-01T00:00:00Z',
+            duration: 3600,
+            'network/data/bytes': 100000,
+            'carbon-operational': 0.034032441600000005,
+            'green-web-host': false,
+            options: {
+              dataReloadRatio: 0.6,
+              firstVisitPercentage: 0.9,
+              returnVisitPercentage: 0.1,
+            },
+          },
+        ]);
+      });
+
       it('throws an error when `type` has wrong value.', async () => {
         const errorMessage =
           "\"type\" parameter is invalid enum value. expected '1byte' | 'swd', received 'wrong'. Error code: invalid_enum_value.";
@@ -168,6 +203,30 @@ describe('lib/co2js: ', () => {
           expect(error).toBeInstanceOf(InputValidationError);
         }
       });
+    });
+
+    it('throws an error if `green-web-host` is neither provided in config nor in input.', async () => {
+      const config = {type: '1byte'};
+      const inputs = [
+        {
+          timestamp: '2021-01-01T00:00:00Z',
+          duration: 3600,
+          'network/data/bytes': 100000,
+        },
+      ];
+
+      expect.assertions(2);
+
+      try {
+        await output.execute(inputs, config);
+      } catch (error) {
+        expect(error).toEqual(
+          new InputValidationError(
+            `Green web host is provided neither in config nor in input.\nConfig: ${config}\nInput: ${inputs[0]}`
+          )
+        );
+        expect(error).toBeInstanceOf(InputValidationError);
+      }
     });
   });
 });
